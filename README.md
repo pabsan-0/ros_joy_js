961f049499c7b677c2a2d7058789fbc75d50cee8
beallamas <bmllamas.ext@catec.aero>
2023-03-29 09:14:45 +0200

    Add vel_topic arg and update readme

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# ROS Joy JS
+# ROS JoyJS
 
 A virtual controller for ROS using NippleJS and Rosbridge.
 
-![image](.fig/demo.gif)
+![image](.fig/demo.png)
 
-It allows you to control simulations from a simple webpapp using Rosbridge.
+It allows you to control an UAV in ROS simulations from a simple webpapp thanks to Rosbridge.
+
 - Move the UAV with the right joystick. 
+
 - Change its orientation using the left joystick.
 
 The controller is executed with roslaunch, like a regular ROS node/pkg, and you can use it from your computer or your phone.
@@ -19,7 +21,7 @@ The controller is executed with roslaunch, like a regular ROS node/pkg, and you 
 
 ## Setup
 
-**Clone the repo inside your ROS project *src* folder**
+**Clone the repo inside your ROS project, in the *src* folder**
 ```
 cd src
 git clone https://bitbucket.org/fadacatec-ondemand/ros_joy_js.git
@@ -49,25 +51,24 @@ roslaunch ros_joy_js run.launch
 
 - **Phone**
 
-You can also control the turtle with your phone:
+You can also control the UAV with your phone:
 
 **1.** Make sure your PC and phone are connected to the same WiFi
 
-**2.** Change the line 4 in the *ros.js* file with your PC IP
+**2.** Change the IP address in the *hostname* file with your PC IP
 
-**3.** Run the webpage executing the *phoneRun.launch* file:
+**3.** Go to the project folder
 
-- Go to the project folder
-
-- Source ROS workspace
+**4.** Source ROS workspace
 ```
 source devel/setup.bash
 ```
-- Execute the *phoneRun.launch* file
+**5.** Execute the *phoneRun.launch* file
 ```
 roslaunch ros_joy_js phoneRun.launch
 ```
+  - If you want to change it, you can pass the topic name as an argument
 
-**4.** Open the `IP:8000` in your phone browser
+**6.** Open the `IP:8000` in your phone browser
 
 **Now you can use your phone as a controller!!!**
--- app/ros.js
+++ app/ros.js
@@ -1,6 +1,7 @@
-/* websocket server connection*/
-
 fetch("hostname").then(res => res.text()).then(ip => {
+    /* websocket server connection*/
+
+    // takes host ip from hostname file and starts connection
     var ros = new ROSLIB.Ros({
         url: `ws://${ip}:9090`
     });
@@ -17,132 +18,137 @@ fetch("hostname").then(res => res.text()).then(ip => {
         console.log('Connection to websocket server closed.');
     });
 
-    // topic to publish uav velocity
-    var cmdVel = new ROSLIB.Topic({
-        ros : ros,
-        name : '/uav_1/ual/set_velocity',
-        messageType : 'geometry_msgs/TwistStamped'
+    // takes arg from phoneRun.launch
+    var topic = new ROSLIB.Param({
+        ros:ros,
+        name:'/phoneRun/vel_topic'
     });
-})
+
+    var cmdVel
+    topic.get(val => {
+        // topic to publish uav velocity
+        cmdVel = new ROSLIB.Topic({
+            ros : ros,
+            name : val,
+            messageType : 'geometry_msgs/TwistStamped'
+        });
+    });
 
 
+    /* uav control actions */
 
-
-/* turtle control actions */
-
-
-
-// function to move the uav, linear velocity msgs publication
-function move(lin_vel_x,lin_vel_y){
-    var msg = new ROSLIB.Message({
-        twist : {
-            linear : {
-            x : lin_vel_x,
-            y : lin_vel_y,
-            z : 0.0
+    // function to move the uav, linear velocity msgs publication
+    function move(lin_vel_x,lin_vel_y){
+        var msg = new ROSLIB.Message({
+            twist : {
+                linear : {
+                x : lin_vel_x,
+                y : lin_vel_y,
+                z : 0.0
+                }
             }
-        }
-    });
+        });
 
-    cmdVel.publish(msg);
-};
-
-// function to rotate the turtle, angular velocity msgs publication
-function rotate(ang_vel_z){
-    var msg = new ROSLIB.Message({
-        twist : {
-            angular : {
-                x : 0.0,
-                y : 0.0,
-                z : ang_vel_z
-            }            
-        }
-    });
-
-    cmdVel.publish(msg);
-}
+        cmdVel.publish(msg);
+    };
 
 
-// joystick to control uav movement
-function movementJoystick (){
+    // function to rotate the uav, angular velocity msgs publication
+    function rotate(ang_vel_z){
+        var msg = new ROSLIB.Message({
+            twist : {
+                angular : {
+                    x : 0.0,
+                    y : 0.0,
+                    z : ang_vel_z
+                }            
+            }
+        });
 
-    //joystick creation
-    const manager1 = nipplejs.create({
-        zone: document.getElementById("zone_joystick_mov"),
-        color: "blue",
-        mode: 'static',
-        position: {left: '50%', top: '50%'},
-    });
-    
-    let pos = {};
-    let interval;
-    
-    lin_vel_x = 0;
-    lin_vel_y = 0;
-    
-    // starts joystick movement
-    manager1.on("start", function(evt, data) {
-        interval = setInterval(function() {
-            move(lin_vel_x,lin_vel_y)
-            console.log(pos);
-        }, 25)
-    });
-    
-    // joystick movement using linear velocity (x,y), takes its position
-    manager1.on("move", function(evt, data) {
-        max_linear = 7.5;
-        max_distance = 75.0;
-        lin_vel_x = Math.cos(data.angle.radian) * max_linear * data.distance/max_distance;
-        lin_vel_y = Math.sin(data.angle.radian) * max_linear * data.distance/max_distance;
-        pos = data.position;
-    });
-    
-    // stops joystick movement 
-    manager1.on("end", function() {
-        if (interval){
-            clearInterval(interval);
-        }
-        move(0,0);
-    })
-}
+        cmdVel.publish(msg);
+    }
 
-// joystick to control turtle orientation
-function orientationJoystick (){
 
-    //joystick creation
-    const manager2 = nipplejs.create({
-        zone: document.getElementById("zone_joystick_or"),
-        color: "red",
-        mode: 'static',
-        position: {left: '50%', top: '50%'},
-    });
-    
-    let interval;
-    
-    ang_vel_z = 0;
-    
-    // starts joystick movement (turtle rotation)
-    manager2.on("start", function(evt, data) {
-        interval = setInterval(function() {
-            rotate(ang_vel_z)
-        }, 25)
-    });
-    
-    // joystick movement using angular velocity (z),
-    manager2.on("move", function(evt, data) {
-        max_angular = 2.0;
-        max_distance = 75.0;
-        ang_vel_z = -Math.cos(data.angle.radian) * max_angular * data.distance/max_distance;
-    });
-    
-    // stops joystick movement (turtle rotation)
-    manager2.on("end", function() {
-        if (interval){
-            clearInterval(interval);
-        }
-        rotate(0);
-    })
-}
+    // joystick to control uav movement
+    function movementJoystick (){
 
-movementJoystick()
-orientationJoystick()
+        //joystick creation
+        const manager1 = nipplejs.create({
+            zone: document.getElementById("zone_joystick_mov"),
+            color: "blue",
+            mode: 'static',
+            position: {left: '50%', top: '50%'},
+        });
+        
+        let interval;
+        
+        lin_vel_x = 0;
+        lin_vel_y = 0;
+        
+        // starts joystick movement
+        manager1.on("start", function(evt, data) {
+            interval = setInterval(function() {
+                move(lin_vel_x,lin_vel_y)
+            }, 25)
+        });
+        
+        // joystick movement using linear velocity (x,y), takes its position
+        manager1.on("move", function(evt, data) {
+            max_linear = 7.5;
+            max_distance = 75.0;
+            lin_vel_x = Math.cos(data.angle.radian) * max_linear * data.distance/max_distance;
+            lin_vel_y = Math.sin(data.angle.radian) * max_linear * data.distance/max_distance;
+            pos = data.position;
+        });
+        
+        // stops joystick movement 
+        manager1.on("end", function() {
+            if (interval){
+                clearInterval(interval);
+            }
+            move(0,0);
+        })
+    }
+
+
+    // joystick to control uav orientation
+    function orientationJoystick (){
+
+        //joystick creation
+        const manager2 = nipplejs.create({
+            zone: document.getElementById("zone_joystick_or"),
+            color: "red",
+            mode: 'static',
+            position: {left: '50%', top: '50%'},
+        });
+        
+        let interval;
+        
+        ang_vel_z = 0;
+        
+        // starts joystick movement (uav rotation)
+        manager2.on("start", function(evt, data) {
+            interval = setInterval(function() {
+                rotate(ang_vel_z)
+            }, 25)
+        });
+        
+        // joystick movement using angular velocity (z),
+        manager2.on("move", function(evt, data) {
+            max_angular = 2.0;
+            max_distance = 75.0;
+            ang_vel_z = -Math.cos(data.angle.radian) * max_angular * data.distance/max_distance;
+        });
+        
+        // stops joystick movement (uav rotation)
+        manager2.on("end", function() {
+            if (interval){
+                clearInterval(interval);
+            }
+            rotate(0);
+        })
+    }
+
+    movementJoystick()
+    orientationJoystick()
+})
